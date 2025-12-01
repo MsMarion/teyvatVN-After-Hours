802a93ea20e95257a811f4310c07454520d8130e
MsMarion <dawnbalaschak@gmail.com>
2025-12-01 01:06:04 -0500

    env host details

--- teyvatVN/frontend/vite.config.js
+++ teyvatVN/frontend/vite.config.js
@@ -1,14 +1,20 @@
 // vite.config.js
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    allowedHosts: process.env.ALLOWED_HOSTS ? process.env.ALLOWED_HOSTS.split(',') : ['dept-spoken-neon-marilyn.trycloudflare.com', 'localhost'],
-    host: process.env.HOST || 'localhost',
-    port: parseInt(process.env.PORT) || 6001
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react(), tailwindcss()],
+    server: {
+      allowedHosts: env.ALLOWED_HOSTS ? env.ALLOWED_HOSTS.split(',') : ['dept-spoken-neon-marilyn.trycloudflare.com', 'localhost'],
+      host: env.HOST || 'localhost',
+      port: parseInt(env.PORT) || 6001
+    }
   }
 });
